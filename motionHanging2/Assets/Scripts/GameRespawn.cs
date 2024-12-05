1782d6dfd43f02c96ed8662093f700115096b102
Shamer27 <144398057+Shamer27@users.noreply.github.com>
2024-12-05 15:29:09 +1100

    Initial Commit + Movement, levels and game logic

--- motionHanging2/Assets/Scripts/GameRespawn.cs
+++ motionHanging2/Assets/Scripts/GameRespawn.cs
@@ -2,15 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameRespawn : MonoBehaviour
-{
-   public float threshold;
 
-   void FixedUpdate()
-   {
-        if(transform.position.y < threshold)
+public class GameRespawn : MonoBehaviour {
+    Vector3 spawnPoint;
+    void OnTriggerEnter (Collider col)
+    {
+        if(col.transform.tag == "death")
         {
-             transform.position = new Vector3(0f, 2.25f, 0f);
+             transform.position = spawnPoint;
         }
-   }
+    }
+
+    void Start() { 
+        spawnPoint =transform.position;
+    }
 }
+
+
